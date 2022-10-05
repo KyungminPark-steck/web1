81e825fb1f509d785670a7761dfe3961692b7409
KyungminPark-steck <110165529+KyungminPark-steck@users.noreply.github.com>
2022-10-05 20:23:28 +0900

    Add files via upload

--- practice3.js
+++ practice3.js
@@ -1,10 +1,10 @@
 // JS 1. 폰켓몬
 function solution(nums) {
     var answer = [];
-    var max = nums.length / 2;
+    var mx = nums.length / 2;
         
     for(let i = 0; i < nums.length; i++) {
-        if(answer.length < max) {
+        if(answer.length < mx) {
             if(!answer.includes(nums[i])) {
                 answer.push(nums[i]);
             }
@@ -21,14 +21,14 @@ function solution(answers) {
     var a2 = [2, 1, 2, 3, 2, 4, 2, 5]
     var a3 = [ 3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
-    var a1c = answers.filter((a,i)=> a === a1[i%a1.length]).length;
-    var a2c = answers.filter((a,i)=> a === a2[i%a2.length]).length;
-    var a3c = answers.filter((a,i)=> a === a3[i%a3.length]).length;
-    var max = Math.max(a1c,a2c,a3c);
+    var a1 = answers.filter((a,i)=> a === a1[i%a1.length]).length;
+    var a2 = answers.filter((a,i)=> a === a2[i%a2.length]).length;
+    var a3 = answers.filter((a,i)=> a === a3[i%a3.length]).length;
+    var max = Math.max(a1,a2,a3);
 
-    if (a1c === max) {answer.push(1)};
-    if (a2c === max) {answer.push(2)};
-    if (a3c === max) {answer.push(3)};
+    if (a1 === max) {answer.push(1)};
+    if (a2 === max) {answer.push(2)};
+    if (a3 === max) {answer.push(3)};
 
 
     return answer;
